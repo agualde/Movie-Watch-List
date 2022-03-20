27ed4de96dd3422dde386e530892cec3c8a96507
Andres Gualde <andres@gualde.com>
2022-03-20 15:54:02 +0100

    not working yet

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,15 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmark = Bookmark.all
+
+    if params[:query].present?
+      # raise
+      @list = @list.global_search(params[:query])
+      respond_to do |format|
+        format.html
+        # format.text { render partial: 'movies/list', locals: { list: @list, bookmark: @bookmark }, formats: [:html] }
+      end
+    end
   end
 
   def new
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,15 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :title, :synopsis ],
+    associated_against: {
+      movie: [ :title, :overview, :english_title, :rating ],
+      bookmark: [ :comment ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
