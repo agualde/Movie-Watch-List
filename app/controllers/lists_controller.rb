145d5795d161c056ffdb85ed23a518e40eb3b8c1
Andres Gualde <andres@gualde.com>
2022-03-21 00:19:38 +0100

    ordered by created at for bookmarks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,11 +5,11 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @bookmarks = Bookmark.where(list_id: @list)
+    @bookmarks = Bookmark.where(list_id: @list).paginate(page: params[:page], per_page: 15).order('bookmarks.created_at DESC')
 
     if params[:query].present?
       # raise
-      @bookmarks = @bookmarks.global_search(params[:query]).paginate(page: params[:page], per_page: 12)
+      @bookmarks = @bookmarks.global_search(params[:query]).paginate(page: params[:page], per_page: 15)
       respond_to do |format|
         format.html
         # format.text { render partial: 'movies/list', locals: { list: @list, bookmark: @bookmark }, formats: [:html] }
