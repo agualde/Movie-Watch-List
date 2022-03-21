3aafb94fa914370e536e1e993c05a9c14b0170ef
Andres Gualde <andres@gualde.com>
2022-03-21 16:09:27 +0100

    switched JS off for perfomance for now

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   
 
   update(event) {
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-    if (this.inputTarget.value != "" ) {
-      fetch(url, { headers: { "Accept": "text/plain" } })
-          .then(response => response.text())
-          .then((data) => {
-          this.listTarget.innerHTML = data
-          })
-  }
+  //   const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+  //   if (this.inputTarget.value != "" ) {
+  //     fetch(url, { headers: { "Accept": "text/plain" } })
+  //         .then(response => response.text())
+  //         .then((data) => {
+  //         this.listTarget.innerHTML = data
+  //         })
+  // }
   }
 }
