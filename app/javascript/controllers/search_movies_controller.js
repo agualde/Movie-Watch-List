595a5c14dc8f8f1296067ba039607c87811d0761
Andres Gualde <andres@gualde.com>
2022-03-23 11:43:40 +0100

    Debouncing JS implemented

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -1,17 +1,25 @@
 import { Controller } from "stimulus"
+import { useDebounce } from 'stimulus-use'
 
 export default class extends Controller {
   static targets = ["form", "input", "list"]
-  
+  static debounces = ["update"]
+
+  connect() {
+    useDebounce(this)
+  }
 
   update(event) {
-  //   const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-  //   if (this.inputTarget.value != "" ) {
-  //     fetch(url, { headers: { "Accept": "text/plain" } })
-  //         .then(response => response.text())
-  //         .then((data) => {
-  //         this.listTarget.innerHTML = data
-  //         })
-  // }
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    if (this.inputTarget.value != "" ) {
+      fetch(url, { headers: { "Accept": "text/plain" } })
+          .then(response => response.text())
+          .then((data) => {
+          this.listTarget.innerHTML = data
+          })
+  }
   }
 }
+
+
+
